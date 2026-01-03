8025369eeb2c8ecbe89e0e2ad751f2a2ae291aaa
pushpa-n220297 <n220297@rguktn.ac.in>
2026-01-03 14:46:13 +0530

    file is updated

---
+++ script.js
@@ -0,0 +1,12 @@
+function gogtc(){
+    window.location.href="gtc.html";
+}
+function gogtb(){
+    window.location.href="gtb.html";
+}
+function gogte(){
+    window.location.href="gte.html";
+}
+function gogtg(){
+    window.location.href="gtg.html";
+}
